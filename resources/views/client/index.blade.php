fec3f013d2ec346f3283bfb8b6ccc4cb350d22f1
hong tham <nguyenlehongtham2005@gmail.com>
2026-03-29 21:00:00 +0700

    update

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1342,3 +1342,19 @@ if (filterKeywordInput) {
     })
 });
   </script>
+  <script>
+  let lastScrollTop = 0;
+  const navbar = document.getElementById("navbar-ctn");
+
+  window.addEventListener("scroll", function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop && scrollTop > 100) {
+      navbar.classList.add("nav-hidden");
+    } else {
+      navbar.classList.remove("nav-hidden");
+    }
+    
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+  }, false);
+</script>

401df3ff7cf7ee214efe3176a4e62204cc3d6331
Ngô Đăng Hiến <168333816+ndhienn@users.noreply.github.com>
2026-04-28 18:38:36 +0700

    6

--- resources/views/client/Login-Register.blade.php
+++ resources/views/client/Login-Register.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/client/include/footer.css">
-    <link rel="stylesheet" href="../../css/client/include/navbar.css">
-    <link rel="stylesheet" href="../../css/client/Login-Register.css">
+    <link rel="stylesheet" href="{{ asset('css/client/include/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/client/include/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/client/Login-Register.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.7.2/css/all.css">
     <title>Login/Register</title>
